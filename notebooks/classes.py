a7b41b82c98e56d662a4970c26c3564eb1fb9d1e
aziart <makhmudov.on@gmail.com>
2023-10-28 22:16:21 +0600

    2.21 (pr 1) answered

---
+++ notebooks/classes.py
@@ -0,0 +1,6 @@
+class Paths:
+    car_train = '../data/car_train.csv'
+    car_test = '../data/car_test.csv'
+    driver_info = '../data/driver_info.csv'
+    fix_info ='../data/fix_info.csv'
+    rides_info ='../data/rides_info.csv'
