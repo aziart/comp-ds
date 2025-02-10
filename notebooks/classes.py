610be21f58177d9d9da2d42cbc682ae2c698b9de
aziart <makhmudov.on@gmail.com>
2025-02-10 22:44:31 +0500

    03.2.11 complete: generated gold feature

--- notebooks/classes.py
+++ notebooks/classes.py
@@ -1,12 +1,14 @@
 class Paths:
-    fen_train = '../data/fen_train.csv'
+    
     car_train = '../data/car_train.csv'
     car_test = '../data/car_test.csv'
     driver_info = '../data/driver_info.csv'
     fix_info ='../data/fix_info.csv'
     rides_info ='../data/rides_info.csv'
+
     stratified_valid ='../data/stratified_valid.csv'
     multi_label ='../data/multi_label.csv'
+
     apply_practice_sample ='../data/03.1.02.csv'
     groupby_practice_sample ='../data/03.1.03.csv'
     merge_practice_sample_cars = "../data/03.1.04_cars.csv"
@@ -15,3 +17,7 @@ class Paths:
     transform_practice_sample_rides_2 = "../data/03.1.06_rides.csv"
     pivot_practice_sample_cars = "../data/03.1.07_cars.csv"
     pr_generate_gold_feature = "../data/03.2.08.csv"
+
+    fen_train = '../data/fen_train.csv'
+    fen_test = '../data/fen_test.csv'
+    fen_sample_solution = '../data/fen_sample_solution.csv'
